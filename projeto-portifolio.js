a4a90716bfcd3532ee80225ecb90966e89672e44
Caio Arlan Gomes <108774432+caioarlan18@users.noreply.github.com>
2022-11-02 19:44:37 -0300

    first commit

---
+++ projeto-portifolio.js
@@ -0,0 +1,18 @@
+function MenuClick() {
+    var itens = document.querySelector('#itens')
+    itens.classList.toggle('active')
+
+}
+function TypeWriter(elemento) {
+    let textoArray = elemento.innerHTML.split('')
+    elemento.innerHTML = ''
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => {
+            elemento.innerHTML += letra
+        }, 95 * i)
+    })
+
+}
+let nome = document.querySelector('h1.nome')
+TypeWriter(nome)
+
